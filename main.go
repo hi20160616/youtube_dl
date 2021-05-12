aec5d1fec4b5b93c8b758d03cb33e9864df910d1
hi20160616 <hi20160616@163.com>
2021-05-12 18:37:36 +0800

    download url by request to web handler pass test

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	ytdl "github.com/kkdai/youtube/v2/downloader"
 )
 
 func main() {
@@ -19,8 +27,49 @@ func main() {
 
 func ytdlHandler(w http.ResponseWriter, r *http.Request) {
 	v := r.URL.Query().Get("v")
-	c := &Client{}
-	if err := c.Download(v); err != nil {
+	q := r.URL.Query().Get("q")
+	if err := Download(v, q); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+// Download will download youtube video by src and qulity,
+// src is the video url or video id,
+// quality can be hd720 or hd1080 etc., default is medium
+func Download(src string, quality string) error {
+	dl := ytdl.Downloader{}
+	// dl.Debug = true
+	v, err := dl.Client.GetVideo(src)
+	if err != nil {
+		return err
+	}
+
+	if quality == "" {
+		quality = "hd720"
+	}
+	vfmt := v.Formats.FindByQuality(quality)
+
+	root, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	dl.OutputDir = filepath.Join(root, "Downloads")
+
+	if strings.HasPrefix(quality, "hd") {
+		if err = checkFFMPEG(); err != nil {
+			return err
+		}
+		return dl.DownloadComposite(context.Background(), "", v, quality, "mp4")
+	}
+
+	return dl.Download(context.Background(), v, vfmt, "")
+}
+
+func checkFFMPEG() error {
+	fmt.Println("check ffmpeg is installed....")
+	if err := exec.Command("ffmpeg", "-version").Run(); err != nil {
+		return fmt.Errorf("please check ffmpegCheck is installed correctly")
+	}
+
+	return nil
 }
---
+++ main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestDownload(t *testing.T) {
+	if err := Download("https://www.youtube.com/watch?v=sxnjarLK5l4", ""); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestYtdlHandler(t *testing.T) {
+	r, err := http.NewRequest("get", "/?v=https://www.youtube.com/watch?v=sxnjarLK5l4", nil)
+	// r, err := http.NewRequest("get", "/?v=https://www.youtube.com/watch?v=sxnjarLK5l4&q=hd720", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	w := httptest.NewRecorder()
+	h := http.HandlerFunc(ytdlHandler)
+	h.ServeHTTP(w, r)
+	if status := w.Code; status != http.StatusOK {
+		t.Error(status)
+	}
+}
